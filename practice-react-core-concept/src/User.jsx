347ea4c047b066baac60914243a7d9e64173748b
khadiza21 <bibikhadiza474@gmail.com>
2024-09-08 00:31:30 +0600

    React JS Practice

--- practice-react-core-concept/src/User.jsx
+++ practice-react-core-concept/src/User.jsx
@@ -1,9 +1,18 @@
-export default function User({user}){
-    const {name,email} = user;
+export default function User({user,handleUser}){
+
+    const userDiv = {
+        border: '2px solid purple',
+        margin: '15px',
+        padding: '15px',
+        borderRadius: '15px'
+    }
+    const {firstname,email} = user;
+ 
 return(
-    <div>
-        <h4>Name: {name } </h4>
+    <div style={userDiv}>
+        <h4>Name: {firstname } </h4>
         <p>Email: {email} </p>
+        <button onClick={() => handleUser(user)}>Mark User</button>
     </div>
 )
 }
--- practice-react-core-concept/src/UsersUseEffect.jsx
+++ practice-react-core-concept/src/UsersUseEffect.jsx
@@ -2,8 +2,17 @@ import { useEffect, useState } from "react";
 import User from "./User";
 
 export default function UsersUseEffect() {
+    const userContainer = {
+
+        display: 'grid',
+        gridTemplateColumns: ' 1fr 1fr 1fr',
+        gridGap: ' 20px',
+
+    }
+  
 
     const [users, setUsers] = useState([]);
+    const [visitedusers, setVisitedUsers] = useState([]);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.org/users')
@@ -11,16 +20,39 @@ export default function UsersUseEffect() {
             .then(data => setUsers(data));
     }, [])
 
+    const handleUser = user => {
+        const newVisitUser = [...visitedusers, user];
+        setVisitedUsers(newVisitUser)
+        console.log('added user');
+        console.log(user)
+    }
+
     return (
         <>
             <div>
                 <h3>Users : {users.length}</h3>
-                {
-                    users.map(user => <User
-                    //key={}
-                    user = {user}
-                    ></User>)
-                }
+                <div>
+                    <h5>Visited countries: {visitedusers.length}</h5>
+                  {  visitedusers.map( visitUser =>  <li
+                  key={visitUser?.login?.uuid}
+                  >{visitUser?.firstname}</li>
+                    )}
+                    {console.log(visitedusers)}
+                </div>
+                <div style={userContainer}>
+                    {
+                        users.map(user =>
+
+                            <User 
+                                key={user.id}
+                                user={user}
+                                handleUser={handleUser}
+                            ></User>
+
+                        )
+                    }
+                </div>
+
             </div>
         </>
     )
